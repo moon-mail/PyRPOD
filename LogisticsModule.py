183d05a4cab80670f3c23873183b405fe84f87a1
andytorrestb <andy@DESKTOP-BTIQS3I.localdomain>
2023-10-30 11:26:13 -0400

    added class to store LM data

---
+++ LogisticsModule.py
@@ -0,0 +1,28 @@
+
+class LogisticsModule:
+
+    def __init__(self, mass, height, radius):
+
+        # Store provided data.
+        self.mass = mass
+        self.height = height
+        self.radius = radius
+
+        self.volume = height * 3.14 * radius **2
+
+        # Calculate moments of inertia.
+        self.I_x = 0.5*mass*radius**2
+        self.I_y = (1.0/12.0)*mass*(height**2 + 3*radius**2)
+        self.I_z = self.I_y
+
+        return
+
+    def add_thruster_config(self, n_thusters, thruster_thrust):
+        # Store provided data.
+        self.n_thusters = n_thusters
+        self.thruster_thrust = thruster_thrust
+        return
+
+    def print_acceleration(self):
+        thrust_total = self.n_thusters * self.thruster_thrust
+        print(thrust_total / self.mass)
--- thrust_torque_estimates.py
+++ thrust_torque_estimates.py
@@ -1,24 +1,18 @@
+from LogisticsModule import LogisticsModule
 import math
 
 # Define mass distrubtion properties.
 m = 30000 # kg
-r = 4.52/2.0 # m
 h = 12 # m
+r = 4.52/2.0 # m
 
-# define usable volume
-v = h * math.pi * r ** 2
-
-# calcualte density (need this?)
-rho = m / v
-
-# moments of intertia
-I_x = 0.5*m*r**2
-I_y = (1.0/12.0)*m*(h**2 + 3*r**2)
-I_z = I_y
-
+# Thruster configuration data.
 n_thusters = 4 # num
 thruster_thrust = 500 # N
-thrust = n_thusters * thruster_thrust
 
-a = thrust / m
-print(a)
+# Instantiate LogisticModule object.
+lm = LogisticsModule(m, h, r)
+
+lm.add_thruster_config(n_thusters, thruster_thrust)
+
+lm.print_acceleration()
