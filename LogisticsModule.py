4655657e07b33ff08a26b3f890ac970ee532aebc
andytorrestb@gmail.com <andy@DESKTOP-BTIQS3I.localdomain>
2023-10-31 23:52:11 -0400

    calculate basic accelerations for individual thrusters

--- LogisticsModule.py
+++ LogisticsModule.py
@@ -1,4 +1,5 @@
 from Vehicle import VisitingVehicle
+import numpy as np
 class LogisticsModule(VisitingVehicle):
 
     def __init__(self, mass, height, radius):
@@ -17,16 +18,39 @@ class LogisticsModule(VisitingVehicle):
 
         return
 
-    # def add_thruster_config(self, path_to_cfg):
-    #     # Store provided data.
-    #     # self.n_thusters = n_thusters
-    #     # self.thruster_thrust = thruster_thrust
-
-    #     self.
-
+    def add_thruster_performance(self, thrust_val):
+        self.thrust = thrust_val
         return
 
+    def calc_thruster_performance(self):
+        
+        for thruster in self.thruster_data:
+            print('thruster id', thruster)
+            print(self.thruster_data[thruster])
+            # Select current thruster from dictionary 
+            cur_thruster = self.thruster_data[thruster]
 
-    # def print_acceleration(self):
-    #     thrust_total = self.n_thusters * self.thruster_thrust
-    #     print(thrust_total / self.mass)
+            # Extract the normal vector 
+            dcm = cur_thruster['dcm']
+            n = [dcm[0][2], dcm[1][2], dcm[2][2]]
+            print('thruster normal vector', n)
+
+            # Calculate thruster force vector
+            F_truster = -1*np.array(n)*self.thrust
+            print('thruster force vector', F_truster)
+            
+            # Calculate acceleration performance 
+            a_x = round(F_truster[0] / self.mass, 3)
+            a_y = round(F_truster[1] / self.mass, 3)
+            a_z = round(F_truster[2] / self.mass, 3)
+            a = np.array([a_x, a_y, a_z])
+            print('resultant translational acceleration', a)
+            
+            
+            # Calculate torque vector
+            r = cur_thruster['exit'][0] # thruster position vector 
+            T_x = F_truster[1]*r[2] + F_truster[2]*r[1]
+            T_y = F_truster[0]*r[2] + F_truster[2]*r[0]
+            T_z = F_truster[0]*r[1] + F_truster[1]*r[0]
+            T = np.array([T_x, T_y, T_z])
+            print('resultant rotational acceleration', T/self.I_x)
--- thrust_torque_estimates.py
+++ thrust_torque_estimates.py
@@ -4,8 +4,8 @@ import math
 
 # Define mass distrubtion properties.
 m = 0.45*30000 # lb converted to kg
-h = 12 # m
-r = 4.52/2.0 # m
+h = 14 # m
+r = 4.0/2.0 # m
 
 # Thruster configuration data (Use previous work).
 n_thusters = 4 # num
@@ -17,15 +17,17 @@ lm = LogisticsModule(m, h, r)
 # Load in thruster configuration data from text file
 lm.add_thruster_config('TCD.txt')
 
-for thruster in lm.thruster_data:
-    print(lm.thruster_data[thruster])
+lm.add_thruster_performance(500)
 
+lm.calc_thruster_performance()
 # Instantiate RPOD calcualtions
 rpod = RPOD(lm.thruster_data)
 
 # Empty arguments default to a "stationary" 6DOF state.
 # i.e. velocity6 and rotiations are zero. 
-rpod.add_current_6dof_state()
-rpod.add_desired_6dof_state()
+# rpod.add_current_6dof_state()
+# rpod.add_desired_6dof_state()
 
-rpod.calc_6dof_performance()
+# rpod.calc_6dof_performance()
+
+# rpod.calc_thruster_performance()
