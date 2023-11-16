5aacef0e198a6ecbda2d0d3238f7c8174be07aa7
Juan P. Roldan <juanproldan2003@gmail.com>
2023-11-15 23:44:18 -0500

    beginning collisionless rocket plume models

---
+++ pyrpod/RarefiedPlumeGasKinetics.py
@@ -0,0 +1,28 @@
+"""
+Nomenclature
+A = normalization constant for a plume model
+D = nozzle diameter, m
+f = Maxwellian velocity distribution function at nozzle exit, s^3/m^3
+Kn = Knudsen number
+n = number density, m^-3
+R_0 = nozzle radius, m
+r = radial direction in a cylindrical coordinate system, m
+S_0 = speed ratio
+T = temperature, K
+U, V, W = macroscopic velocity components, m/s
+u, v, w = thermal velocity components, m/s
+V_r = velocity component along the radial direction in a spherical coordinate system, m/s
+X, Y, Z = cartesian coordinates, m
+beta = 1/2RT, sec^2/m^2
+gamma = specific heat ratio
+epsilon = angular or azimuthal angle
+theta = arctan(sqrt(Y^2 + Z^2)/X), general zenith angle with general point coordinates (X, Y, Z)
+theta_max = limiting turning angle
+kappa = plume model beaming exponent
+rho_s = plume model nozzle throat density, kg/m3
+psi = arctan(Z/X), specific zenith angle formed by points (0,0,0), (X, 0, Z), and x axis
+Omega = integral domain
+0 = properties at nozzle exit
+1 = flowfield properties
+' = simplified analytical results
+"""
