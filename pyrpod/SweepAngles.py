b439fb1d89efa43521e77022ad7abd2237006152
Juan Pablo Roldan <juanpr@DESKTOP-9L2JRP8.localdomain>
2023-12-22 21:06:22 -0500

    fixed sweep limits

--- pyrpod/SweepAngles.py
+++ pyrpod/SweepAngles.py
@@ -2,14 +2,29 @@ import numpy as np
 
 class SweepAngles:
 
+    #TODO need to add the actual loop to grab all the dcms, maybe pitches/yaws array 
+
+    #TODO dont use instance variables. EACH thruster has its own init_pitch and init_yaw
+    #or maybe use a lise of init_pitches abd init_yaws
     def __init__(self, DCM):
 
         self.init_pitch, self.init_yaw = self.calculate_init_angles(DCM)
 
+    def standardize_thruster_normal():
+
     def calculate_init_angles(self, DCM):
 
-        pitch = np.arcsin(DCM[2][2])
-        yaw = np.arcsin(DCM[1][2] / np.cos(pitch))
+        z_x = DCM[0][2]
+        z_y = DCM[1][2]
+        z_z = DCM[2][2]
+
+        n_t_mag = np.sqrt(z_x ** 2 + z_y ** 2 + z_z ** 2)
+
+        pitch = np.arcsin(z_z / n_t_mag)
+        if(z_y == 0):
+            yaw = np.arccos((z_x / n_t_mag) / np.cos(pitch))
+        else:
+            yaw = np.arcsin((z_y / n_t_mag) / np.cos(pitch))
 
         pitch = np.rad2deg(pitch)
         yaw = np.rad2deg(yaw)
@@ -18,7 +33,7 @@ class SweepAngles:
 
     #[x_t; y_t; z_t] = [z_x; z_y; z_z] = [DCM][0; 0; 1]
     def calculate_DCM(self, pitch, yaw):
-        
+
         pitch += self.init_pitch
         yaw += self.init_yaw
 
@@ -34,65 +49,142 @@ class SweepAngles:
         return DCM
 
     #with the Y and Z coords of a thruster, can determine the limits of a sweep in deg
-    def longitudinal_angle_limits(r, y, z):
+    def pos_long_angle_limits(r, y, z):
 
-        if(y >= 0 and z >= 0): #Q1
-            pitch_max = 45
-            pitch_min = -0.5 * np.rad2deg(np.arccos(z/r))
-            yaw_max = 45
-            yaw_min = -0.5 * np.rad2deg(np.arcsin(z/r))
-            return pitch_max, pitch_min, yaw_max, yaw_min
-        
-        if(z >= 0): #Q2
-            pitch_max = 45
-            pitch_min = -0.5 * np.rad2deg(np.arccos(z/r))
-            yaw_max = 0.5 * np.rad2deg(np.arcsin(z/r))
-            yaw_min = -45
-            return pitch_max, pitch_min, yaw_max, yaw_min
+        if(y == r):
+            pitch_min, pitch_max = -45, 45
+            yaw_min, yaw_max = -45, 0
+            return pitch_min, pitch_max, yaw_min, yaw_max
 
-        if(y <= 0): #Q3
-            pitch_max = 0.5 * (180 - np.rad2deg(np.arcos(z/r)))
-            pitch_min = -45
-            yaw_max = -0.5 * np.rad2deg(np.arcsin(z/r))
-            yaw_min = -45
-            return pitch_max, pitch_min, yaw_max, yaw_min
+        if(z == r):
+            pitch_min, pitch_max = 0, 45
+            yaw_min, yaw_max = -45, 45
+            return pitch_min, pitch_max, yaw_min, yaw_max
         
-        #Q4
-        pitch_max = 0.5 * (180 - np.rad2deg(np.arccos(z/r)))
-        pitch_min = -45
-        yaw_max = 45
-        yaw_min = 0.5 * np.rad2deg(np.arcsin(z/r))
-        return pitch_max, pitch_min, yaw_max, yaw_min
+        if(-y == r):
+            pitch_min, pitch_max = -45, 45
+            yaw_min, yaw_max = 0, 45
+            return pitch_min, pitch_max, yaw_min, yaw_max
+        
+        if(-z == r):
+            pitch_min, pitch_max = -45, 0
+            yaw_min, yaw_max = -45, 45
+            return pitch_min, pitch_max, yaw_min, yaw_max
+        
+        #on Q1
+        if(z > 0 and y > 0):
+            pitch_min, pitch_max = 0, 45
+            yaw_min, yaw_max = -45, 0
+            return pitch_min, pitch_max, yaw_min, yaw_max
+        
+        #on Q2
+        if(z > 0):
+            pitch_min, pitch_max = 0, 45
+            yaw_min, yaw_max = 0, 45
+            return pitch_min, pitch_max, yaw_min, yaw_max
+        
+        #on Q3
+        if(y < 0):
+            pitch_min, pitch_max = -45, 0
+            yaw_min, yaw_max = 0, 45
+            return pitch_min, pitch_max, yaw_min, yaw_max
+        
+        #on Q4
+        pitch_min, pitch_max = -45, 0
+        yaw_min, yaw_max = -45, 0
+        return pitch_min, pitch_max, yaw_min, yaw_max
     
-    def lateral_angle_limits(r, y, z):
+    #with the Y and Z coords of a thruster, can determine the limits of a sweep in deg
+    def neg_long_angle_limits(r, y, z):
 
-        yaw_max = 45
-        yaw_min = -45
+        if(y == r):
+            pitch_min, pitch_max = -45, 45
+            yaw_min, yaw_max = 0, 45
+            return pitch_min, pitch_max, yaw_min, yaw_max
 
+        if(z == r):
+            pitch_min, pitch_max = 0, 45
+            yaw_min, yaw_max = -45, 45
+            return pitch_min, pitch_max, yaw_min, yaw_max
+        
+        if(-y == r):
+            pitch_min, pitch_max = -45, 45
+            yaw_min, yaw_max = -45, 0
+            return pitch_min, pitch_max, yaw_min, yaw_max
+        
+        if(-z == r):
+            pitch_min, pitch_max = -45, 0
+            yaw_min, yaw_max = -45, 45
+            return pitch_min, pitch_max, yaw_min, yaw_max
+        
+        #on Q1
+        if(z > 0 and y > 0):
+            pitch_min, pitch_max = 0, 45
+            yaw_min, yaw_max = 0, 45
+            return pitch_min, pitch_max, yaw_min, yaw_max
+        
+        #on Q2
+        if(z > 0):
+            pitch_min, pitch_max = 0, 45
+            yaw_min, yaw_max = -45, 0
+            return pitch_min, pitch_max, yaw_min, yaw_max
+        
+        #on Q3
+        if(y < 0):
+            pitch_min, pitch_max = -45, 0
+            yaw_min, yaw_max = -45, 0
+            return pitch_min, pitch_max, yaw_min, yaw_max
+        
+        #on Q4
+        pitch_min, pitch_max = -45, 0
+        yaw_min, yaw_max = 0, 45
+        return pitch_min, pitch_max, yaw_min, yaw_max
+
+    def lat_angle_limits(r, y, z):
+
+        yaw_min, yaw_max = -45, 45
+
+        if(y == r):
+            pitch_min, pitch_max = -45, 45
+            return pitch_min, pitch_max, yaw_min, yaw_max
+        
+        if(z == r):
+            pitch_min, pitch_max = 0, 45
+            return pitch_min, pitch_max, yaw_min, yaw_max
+        
+        if(-y == r):
+            pitch_min, pitch_max = -45, 45
+            return pitch_min, pitch_max, yaw_min, yaw_max
+        
+        if(-z == r):
+            pitch_min, pitch_max = -45, 0
+            return pitch_min, pitch_max, yaw_min, yaw_max
+        
         #Q1 and Q2
-        if(z >= 0):
+        if(z > 0):
+            pitch_min = -0.5 * np.arccos(z/r)
             pitch_max = 45
-            pitch_min = -0.5 * (np.rad2deg(np.arcos(z/r)))
+            return pitch_min, pitch_max, yaw_min, yaw_max
         
         #Q3 and Q4
-        else:
-            pitch_max = 0.5 * (180 - np.rad2deg(np.arccos(z/r)))
-            pitch_min = -45
-        
-        return pitch_max, pitch_min, yaw_max, yaw_min
-    
-    def vertical_angle_limits(r, y, z):
-
-        pitch_max = 45
         pitch_min = -45
+        pitch_max = 0.5 * (180 - np.rad2deg(np.arccos(z/r)))
+        return pitch_min, pitch_max, yaw_min, yaw_max
+    
+#arguably, the vert thrusters should not be allowed to yaw. issues with asymmetry.
+    def vert_angle_limits(r, y, z):
+
+        pitch_min, pitch_max = -45, 45
+
+        if(np.abs(z) == r):
+            yaw_min, yaw_max = -180, 180
+            return pitch_min, pitch_max, yaw_min, yaw_max
 
         #Q1 and Q4
         if(y >= 0):
-            yaw_max = 45
-            yaw_min = -0.5 * np.rad2deg(np.arcsin(z/r))
-            return pitch_max, pitch_min, yaw_max, yaw_min
+            yaw_min, yaw_max = 0, 180
+            return pitch_min, pitch_max, yaw_min, yaw_max
         
         #Q2 and Q3
-        yaw_max = 0.5 * np.rad2deg(np.arcsin(z/r))
-        yaw_min = -45
-        return pitch_max, pitch_min, yaw_max, yaw_min
+        yaw_min, yaw_max = -180, 0
+        return pitch_min, pitch_max, yaw_min, yaw_max
