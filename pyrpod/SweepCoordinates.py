ec56aa3914488f3a11ee0433cdcd4c63efa593ed
Juan P. Roldan <juanproldan2003@gmail.com>
2024-01-10 15:56:56 -0500

    restructured to handle dicts rather than .txt

--- pyrpod/SweepCoordinates.py
+++ pyrpod/SweepCoordinates.py
@@ -1,71 +1,21 @@
-import os
+'''
+sweep coords takes a dictionary of thruster configurations and produces an array of 
+configurations swept over the inputted x-coords
+'''
+
 
 class SweepCoordinates:
 
-    #save a dictionary with keys being the unique x-coors where thrusters are placed
-    #the values being the line number in the TCD where a thruster in that respective coord is
-    def group_thruster_rows(self, thruster_config_file):
-        try:
-            #read TCD
-            with open(thruster_config_file, 'r') as file:
-                lines=file.readlines()
+    def sweep_coords(config, x0, xf, dx):
 
-            coord_dict = {}
-            for line_number, line in enumerate(lines, 1):
-                cols = line.strip().split(' ')
-                if len(cols) < 5:
-                    continue
-                coord = float(cols[2])
-                if coord not in coord_dict:
-                    coord_dict[coord] = [line_number]
-                else:
-                    coord_dict[coord].append(line_number)
-            return coord_dict        
-            
-        except FileNotFoundError:
-            print("File " + thruster_config_file + " not found.")
+        configs_swept_coords = []
 
-    #given a TCD file, and some increment, 
-    #assumes TCD file has organized thrusters by x-coord. with a maximum of two adjustable rows
-    #(+)ive delta_x moves the thrusters back (more negative x-coord), opposite for (-)ive delta_x
-    def x_coord_sweep(self, thruster_config_file, del_x1, del_x2, height_LM):
-        coord_dict = self.group_thruster_rows(thruster_config_file)
-        #create output folder
-        output_folder = 'sweptTCD'
-        os.makedirs(output_folder, exist_ok=True)
+        for x_pos in range(x0, xf, dx):
 
-        try:
-            #read TCD
-            with open(thruster_config_file, 'r') as file:
-                lines=file.readlines()
+            for thruster in config:
+                config[thruster]['exit'][0] += dx
 
-            increment1 = 0
-            increment2 = 0
-            
-            coord2, trash = coord_dict.popitem()
-            coord1, trash = coord_dict.popitem()
-            print(coord_dict)
+            configs_swept_coords.append(config)
+        
+        return configs_swept_coords
 
-            while 0 > coord1 > -height_LM:
-                if coord2 is not None:
-                    new_coord2 = coord2
-                    while 0 > new_coord2 > -height_LM:
-                        new_coord2 -= increment2
-                        formatted_coord2 = "{:.6e}".format(new_coord2)
-                        line_numbers_to_edit = coord_dict.get(str(coord2), [])
-                        for line_number in line_numbers_to_edit:
-                            cols = lines[line_number - 1].split()
-                            cols[2] = formatted_coord2
-                            lines[line_number - 1] = ' '.join(cols) + '\n'
-                        increment2 += del_x2
-                        with open(thruster_config_file, 'w') as file:
-                            file.writelines(lines)
-                increment1 += del_x1
-                coord1 -= increment1
-                
-
-        except FileNotFoundError:
-            print("File " + thruster_config_file + " not found.")
-
-obj = SweepCoordinates()
-obj.x_coord_sweep('../data/tcd/TCD2.txt', 2, -2, 14)
