875ef4f788d339a8beed43f764ed16da8951edbe
Juan P. Roldan <juanproldan2003@gmail.com>
2024-01-17 14:18:03 -0500

    added comments and test case

--- pyrpod/SweepCoordinates.py
+++ pyrpod/SweepCoordinates.py
@@ -1,17 +1,33 @@
-'''
-sweep coords takes a dictionary of thruster configurations and produces an array of 
-configurations swept over the inputted x-coords
-'''
-
 import copy
 
-#made a copy, new config, and yet still facing overwrite issue???
+'''
+    sweep_coords()
+        input parameters: self, config, x0, xf, dx
+'''
 class SweepCoordinates:
+
+    '''
+    Class responsible for axially sweeping a ring of thrusters in a given configuration
+
+    sweep_coords()
+    --------------
+    inputs: self; thruster configuration <dict>; x0, xf, dx <float>
+    outputs: configs_swept_coords <array>, where each element is a config dict per iteration of the sweep
+
+    read_swept_coords()
+    -------------------
+    inputs: self, swept configurations <array>
+    outputs: prints to terminal the position of the ring of thrusters per saved configuration
+    '''
 
     def sweep_coords(self, config, x0, xf, dx):
 
+        # to store all the configs
         configs_swept_coords = []
 
+        # per step of the sweep, make a new config object
+        # copy over the config and simply modify each thrusters position
+        # append the config to the list
         for x_pos in range(x0, xf+dx, dx):
             new_config = copy.deepcopy(config)
             for thruster in config:
@@ -21,28 +37,12 @@ class SweepCoordinates:
         
         return configs_swept_coords
 
-dcm = [[0, 0, 1], [0, 0, 0], [0, 0, 0]]
-#why are names and types in an array???
-config = {
-    'P1T1': {'name': ['P1T1'], 'type': ['001'], 'exit': [-1, 2.5, 0], 'dcm': dcm}, 
-    'P2T1': {'name': ['P2T1'], 'type': ['001'], 'exit': [-1, 0, 2.5], 'dcm': dcm},
-    'P3T1': {'name': ['P3T1'], 'type': ['001'], 'exit': [-1, -2.5, 0], 'dcm': dcm}, 
-    'P4T1': {'name': ['P4T1'], 'type': ['001'], 'exit': [-1, 0, -2.5], 'dcm': dcm}
-}
+    def read_swept_coords(self, swept_configs):
 
-thruster_groups = {
-    '+x': [],
-    '-x': ['P1T1', 'P2T1', 'P3T1', 'P4T1'],
-    '+y': [],
-    '-y': [],
-    '+z': [],
-    '-z': [],
-    '+pitch': ['P4T1'],
-    '-pitch': ['P2T1'],
-    '+yaw' : ['P3T1'],
-    '-yaw' : ['P1T1']
-}
-
-test = SweepCoordinates()
-config_array = test.sweep_coords(config, 0, -14, -1)
-print(config_array)
+        # per saved config, print the position of each thruster
+        # to quickly identify the position of the swept ring
+        for i, config in enumerate(swept_configs):
+            print(f'Config #{i}:')
+            for thruster in config:
+                print(f'{thruster}: {config[thruster]["exit"] }')
+            print(f'\n')
