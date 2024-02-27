da931d995df60d968474f22af19a671e86e79141
Nick Palumbo <nickpalumbo8@gmail.com>
2024-02-26 19:52:40 -0500

    Edited config.ini and transform module for multi_thrusters_square case

--- case/multi_thrusters_square/stl/transform_square_plate.py
+++ case/multi_thrusters_square/stl/transform_square_plate.py
@@ -1,10 +1,9 @@
 from stl import mesh
 import math
 
-mesh = mesh.Mesh.from_file('square_plate_refinedx3.stl')
+mesh = mesh.Mesh.from_file('square_plate.stl')
 
-mesh.points = 0.001 * mesh.points
 
 mesh.translate([-1/2, -12, -12])
 
-mesh.save('square_plate_refinedx3_transformed.stl')
+mesh.save('square_plate_transformed.stl')
