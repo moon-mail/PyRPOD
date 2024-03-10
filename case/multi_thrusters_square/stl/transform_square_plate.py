a823f9a6c84fa316fdaef49eb4c43db521e230cf
Nick Palumbo <nickpalumbo8@gmail.com>
2024-03-09 23:36:13 -0500

    multi thrusters square

---
+++ case/multi_thrusters_square/stl/transform_square_plate.py
@@ -0,0 +1,10 @@
+from stl import mesh
+import math
+
+mesh = mesh.Mesh.from_file('square_plate_defaced.STL')
+
+mesh.points = 1000 * mesh.points
+
+mesh.translate([-1/2, -12, -12])
+
+mesh.save('square_plate_defaced_transformed.STL')
