a7220e7bf832d5e6ba4492c267cd9cab4eef69e1
Nick Palumbo <nickpalumbo8@gmail.com>
2024-03-11 18:05:53 -0400

    plume_case: removed defaced stls, updated config.ini to have file extension stl instead of STL, updated transform function, tested results

--- case/plume_case/stl/transform_flat_plate.py
+++ case/plume_case/stl/transform_flat_plate.py
@@ -1,10 +1,8 @@
 from stl import mesh
 import math
 
-mesh = mesh.Mesh.from_file('flat_plate_defaced.STL')
-
-mesh.points = 1000 * mesh.points
+mesh = mesh.Mesh.from_file('flat_plate_low_res.stl')
 
 mesh.translate([-1/2, -70/2, -10/2])
 
-mesh.save('flat_plate_defaced_transformed.STL')
+mesh.save('flat_plate_low_res_transformed.stl')
