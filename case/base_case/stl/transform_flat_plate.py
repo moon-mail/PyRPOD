bcde9ef097fda0a539492402d591f384a045f3b6
Nick Palumbo <nickpalumbo8@gmail.com>
2024-03-10 20:45:06 -0400

    test_all.py changes: decreased mesh fidelity for multi_thrusters_square, base_case, and plume_case to lower run time. disabled progress bars in RPOD.py to stop test_all.py error.

---
+++ case/base_case/stl/transform_flat_plate.py
@@ -0,0 +1,10 @@
+from stl import mesh
+import math
+
+mesh = mesh.Mesh.from_file('flat_plate_low_res_defaced.STL')
+
+mesh.points = 1000 * mesh.points
+
+mesh.translate([-1/2, -70/2, -10/2])
+
+mesh.save('flat_plate_low_res_defaced_transformed.STL')
--- case/multi_thrusters_square/stl/transform_square_plate.py
+++ case/multi_thrusters_square/stl/transform_square_plate.py
@@ -1,10 +1,10 @@
 from stl import mesh
 import math
 
-mesh = mesh.Mesh.from_file('square_plate_defaced.STL')
+mesh = mesh.Mesh.from_file('square_plate_low_res_defaced.STL')
 
 mesh.points = 1000 * mesh.points
 
 mesh.translate([-1/2, -12, -12])
 
-mesh.save('square_plate_defaced_transformed.STL')
+mesh.save('square_plate_low_res_defaced_transformed.STL')
