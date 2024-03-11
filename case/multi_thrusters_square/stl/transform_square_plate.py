ac98d1ca72c24805061539a43b687144cdcc8a50
Nick Palumbo <nickpalumbo8@gmail.com>
2024-03-11 18:00:56 -0400

    multi_thrusters_square: removed defaced stls, updated config.ini to have file extension stl instead of STL, updated transform function, tested results

--- case/multi_thrusters_square/stl/transform_square_plate.py
+++ case/multi_thrusters_square/stl/transform_square_plate.py
@@ -1,10 +1,8 @@
 from stl import mesh
 import math
 
-mesh = mesh.Mesh.from_file('square_plate_low_res_defaced.STL')
-
-mesh.points = 1000 * mesh.points
+mesh = mesh.Mesh.from_file('square_plate_low_res.stl')
 
 mesh.translate([-1/2, -12, -12])
 
-mesh.save('square_plate_low_res_defaced_transformed.STL')
+mesh.save('square_plate_low_res_transformed.stl')
