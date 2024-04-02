6d864ea11b1f038885a533e2154a255e15597c7c
andytorrestb <andytorrestb@gmail.com>
2024-04-01 20:17:26 -0400

    WIP added initial structure for trade study analysis

--- tests/mdao/mdao_verification_test_01.py
+++ tests/mdao/mdao_verification_test_01.py
@@ -7,7 +7,7 @@
 # ========================
 # PyRPOD: test/mdao/mdao_verification_test_01.py
 # ========================
-# Write test case description.
+# Initial trade study used to explore a design space and inform optimization efforts of a single thruster configuration.
 
 import test_header
 import unittest
@@ -15,7 +15,53 @@ import unittest
 class MDAOTest(unittest.TestCase):
     def test_mdao(self):
 
-        # print("mdao verification test")
+    # 1. Set Up
+        # Load in Fixed LM and thruster configuation for trade study.
+        case_dir = '../case/trade_study'
+
+        # Load Target Vehicle.
+        tv = TargetVehicle.TargetVehicle(case_dir)
+        tv.set_stl()
+
+        # Instantiate LogisticModule object.
+        lm = LogisticsModule.LogisticsModule(case_dir)
+
+        # Define LM mass distribution properties.
+        m = 14000 # kg
+        h = 11 # m
+        r = 2 # m
+        lm.set_inertial_props(m, h, r)
+
+        # Load in thruster configuration.
+        lm.set_thruster_config()
+        lm.set_thruster_metrics()
+        lm.assign_thruster_groups()    
+ 
+        # Determine design variables to vary over. 
+        axial_overshoot = [0, 25, 50, 75, 100] # m/s (WIP, replace with physical values)
+        axial_thruster_pos = [0, 2.75, 5.5, 8.25, 11] # m (ignoring solar panel since decel thrusters)
+        surface_cant_angle = [0, 15, 30, 45, 60] # degrees
+
+        sweep_vars = {
+            'axial_overshoot': axial_overshoot,
+            'axial_thruster_pos': axial_thruster_pos,
+            'surface_cant_angle': surface_cant_angle
+        }
+
+
+    # 2. Excecute
+        # Produce data for trade study by running docking analysis according to relevant design variable sweeps.
+        study = TradeStudy.TradeStudy(case_dir)
+        results = study.run_var_sweep(sweep_vars, lm, tv)
+
+
+        # Post process results and perform trade studies analysis.
+        design_metrics = ['fuel_usage', 'plume', 'maneuver', 'safety']
+        ideal_configs = study.process_results(design_metrics, results)
+
+    # 3. Assert
+        # TBD. This will be developed at the very end to lock in desired results given 
+
         return
 
 if __name__ == '__main__':
